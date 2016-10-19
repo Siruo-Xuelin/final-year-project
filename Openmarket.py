ee41d818bf7ed5242feafeb567f7e4eae9fee4f5
Siruo-Xuelin <Xuelin.Wang13@student.xjtlu.edu.cn>
2016-10-20 01:27:18 +0800

    create a function to make  sure the stock market is open

---
+++ Openmarket.py
@@ -0,0 +1,17 @@
+import time
+import datetime
+
+class Openmarket():
+    
+    def __init__(self, t1):
+        self._tt = t1
+        
+    
+    def judgeWeekend(self,t):    
+        t = time.strptime(self._tt, '%Y-%m-%d') 
+        y,m,d = t[0:3]
+        a = datetime.datetime.weekday(datetime.datetime(y,m,d))
+        if (a+1>5):
+            print False
+        else:
+            print a   
