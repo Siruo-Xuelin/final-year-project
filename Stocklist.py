2b01612477e489e88228bd3bce1bc826af35a312
Siruo-Xuelin <Xuelin.Wang13@student.xjtlu.edu.cn>
2017-02-22 22:36:57 +0800

    simplify the process of getting the volume

--- Stocklist.py
+++ Stocklist.py
@@ -7,29 +7,25 @@ import Openmarket
 
 class StockList():
     
-    def __init__(self):
+    def __init__(self, selectiondate):
         df=pd.read_csv('S&P500index.csv')
-        stocks = df.iloc[:, 0].tolist()
-        volumes=([None]*len(stocks))
-        now_time = datetime.datetime.now()
-        y_time = now_time + datetime.timedelta(days=-1)
-        y_time_nyr = y_time.strftime('%Y-%m-%d')
+        stocks = df['Symbol']
+        #now_time = datetime.datetime.now()
+        #y_time = now_time + datetime.timedelta(days=-1)
+        #y_time_nyr = y_time.strftime('%Y-%m-%d')
         
+        df1=web.DataReader(stocks, 'yahoo', selectiondate, selectiondate)
+        df2 = df1['Volume'].T
+        #volumes = df2[selectiondate]
+        #s = df2.axes
 
 
-        for i in range(len(volumes)):
-            if (Openmarket.Openmarket(y_time_nyr, stocks[i]).judgeTime() == False):
-                volumes[i] = 0
-            else: 
-                volumes[i] = web.DataReader(stocks[i], 'yahoo', y_time_nyr, y_time_nyr).Volume[y_time_nyr]
-                    
-        self._df = pd.DataFrame({'stock': stocks, 'volume':volumes})
-        self._df = self._df.set_index('stock')
-        
+        self._df = df2
+        self._seleciondate = selectiondate
 
     def topNVolume(self, n = 5, order = False, minVolume = 0):
         if minVolume == 0:
-            r = self._df.sort_values(by='volume', ascending=False)[:n]
+            r = self._df.sort_values(by=self._seleciondate, ascending=False)[:n]
             if order:
                 return r
             else:
