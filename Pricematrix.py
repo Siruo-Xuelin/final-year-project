db8250d8ca3234b63e89b2cae95bfb188cf06366
Siruo-Xuelin <Xuelin.Wang13@student.xjtlu.edu.cn>
2017-02-22 18:36:16 +0800

    Simplify the process of getting price data

--- Pricematrix.py
+++ Pricematrix.py
@@ -1,33 +1,13 @@
 import pandas_datareader.data as web 
 import pandas as pd
-import numpy as np
-import Openmarket
-import time
-import datetime
 
 
 class PriceMatrix():
 
-    def __init__(self, start, pe):
+    def __init__(self, start, end):
         
         df1=pd.read_csv('S&P500index.csv')
-        stocks = df1.iloc[:, 0].tolist()
-
-        dates = pd.date_range(start,periods=pe)
-        dates_st = [None]*len(dates)
-        
-        for p in range (len(dates)):
-            dates_st[p] = dates[p].strftime('%Y-%m-%d')
-        
-        df2 = pd.DataFrame(0, index=stocks, columns=dates_st)
-        
-        for i in range(len(stocks)):
-            for j in range (len(dates_st)):
-                if (Openmarket.Openmarket(dates_st[j], stocks[i]).judgeTime() == True):
-                    df2.iloc[i,j] = web.DataReader(stocks[i], 'yahoo', dates_st[j] , dates_st[j]).Close[dates_st[j]]
-                else:
-                    df2.iloc[i,j] = 'None'
-
-
-
-        df2.to_csv('pricedata.csv')
+        stocks = df1['Symbol']
+        df2=web.DataReader(stocks, 'yahoo', start,end)
+        df3=df2['Adj Close'].T
+        df3.to_csv('pricedata.csv')
